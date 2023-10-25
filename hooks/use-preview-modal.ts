22b87fa50c8d876160adfcca4016012d7fce0fd9
jiangruijian <jrj444@foxmail.com>
2023-10-25 14:13:32 +0800

    add a modal component

---
+++ hooks/use-preview-modal.ts
@@ -0,0 +1,18 @@
+import { Product } from "@/type";
+import { create } from "zustand";
+
+interface previewModalStore {
+  isOpen: boolean;
+  data?: Product;
+  onOpen: (data: Product) => void;
+  onClose: () => void;
+}
+
+const usePreviewModal = create<previewModalStore>((set) => ({
+  isOpen: false,
+  data: undefined,
+  onOpen: (data: Product) => set({ data, isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default usePreviewModal;
